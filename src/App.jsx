20d993a40a4525dba9fe7d3ca9509b1007f8f030
Regiaaaaaa <gheizarafashaquille@gmail.com>
2026-01-26 11:38:59 +0700

    feat: add url all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
-import LoginPetugas from './pages/auth/petugas/Login'
-import RegisterSiswa from "./pages/auth/siswa/Register";
-import LoginSiswa from "./pages/auth/siswa/Login";
-import RegisterStaff from "./pages/auth/staff/Register";
-import LoginStaff from "./pages/auth/staff/Login";
+import Login from './pages/auth/Login'
+import PortalRegister from "./pages/auth/PortalRegister";
+import RegisterSiswa from "./pages/auth/RegisterSiswa";
+import RegisterStaff from "./pages/auth/RegisterStaff";
 import AdminDashboard from "./pages/admin/Dashboard";
 import OperatorDashboard from "./pages/operator/Dashboard";
 import UserDashboard from './pages/users/Dashboard';
 
+import ScrollToTop from './components/ScrollToTop'; // ⬅️ TAMBAH INI
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> 
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login/petugas" element={<LoginPetugas />} />
-        <Route path="/login/siswa" element={<LoginSiswa />} />
-        <Route path="/login/staff" element={<LoginStaff />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/portal/register" element={<PortalRegister />} />
         <Route path="/register/siswa" element={<RegisterSiswa />} />
         <Route path="/register/staff" element={<RegisterStaff />} />
 
-
-         <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/operator/dashboard" element={<OperatorDashboard />} />
         <Route path="/users/dashboard" element={<UserDashboard />} />
       </Routes>
